bd4e59862ca8b5beb672b55ddd71d204be45c24b
Marcel Donges <marcel.donges@PI400.local>
2022-11-09 11:26:15 +0100

    minor changes

--- once.sh.ts/test/environment/docker/ubuntu/22.04.sshd/entrypoint/build.sh
+++ once.sh.ts/test/environment/docker/ubuntu/22.04.sshd/entrypoint/build.sh
@@ -5,6 +5,7 @@ echo "Starting custom build script: $0"
 apt update && apt install  net-tools wget openssh-server sudo -y
 useradd -rm -d /home/ubuntu -s /bin/bash -g root -G sudo -u 1000 test 
 echo 'test:test' | chpasswd
+echo 'test:test' | sudo chpasswd
 service ssh start
 
 # generate server keys
