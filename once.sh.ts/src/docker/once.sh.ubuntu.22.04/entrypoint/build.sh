f012b4e642bec00d6f9ae19e0cd5e8366d9e7cb0
Matthias Kittner <consulting@matthiaskittner.de>
2023-01-28 12:11:55 +0100

    Install and start once for localhost:8080

--- once.sh.ts/src/docker/once.sh.ubuntu.22.04/entrypoint/build.sh
+++ once.sh.ts/src/docker/once.sh.ubuntu.22.04/entrypoint/build.sh
@@ -1,23 +1,45 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
 echo "Starting custom build script: $PWD $0"
 export OOSH_SSH_CONFIG_HOST="dockerSSH.once2023"
-/root/entrypoint/once.sh
+
+# Temporary log settings
+#export LOG_LEVEL=3
+#export LOG_DEVICE="$HOME/build-initial-oosh.log"
+
+# Remove, when sudo in oosh is fixed
+apt install git -y
+
+# Download and install oosh
+env sh -c "$(wget -O- https://raw.githubusercontent.com/Cerulean-Circle-GmbH/once.sh/main/init/oosh)"
+source ~/config/user.env
 
 echo "custom build script: $PWD $0"
 echo "====== DONE ================="
 
-apt update && apt install  net-tools wget openssh-server sudo -y
-useradd -rm -d /home/ubuntu -s /bin/bash -g root -G sudo -u 1000 test 
-echo 'test:test' | chpasswd
-echo 'root:test' | sudo chpasswd
-service ssh start
+### migrate this code into the state machine
+
+# Install stuff
+oo cmd net-tools
+oo cmd openssh-server
+oo cmd errno
+
+# Add test user
+#useradd -rm -d /home/ubuntu -s /bin/bash -g root -G sudo -u 1000 test 
+#echo 'test:test' | chpasswd
+#echo 'root:test' | sudo chpasswd
 
 # generate server keys
-ssh-keygen -A
-#RUN ssh-keygen -t dsa -N "my passphrase" -C "test key" -f mykey
-#ssh-keygen -t rsa -q -f "$HOME/.ssh/id_rsa" -N ""
+#ssh-keygen -A
+##RUN ssh-keygen -t dsa -N "my passphrase" -C "test key" -f mykey
+##ssh-keygen -t rsa -q -f "$HOME/.ssh/id_rsa" -N ""
 
 # allow root to login
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+#sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+#sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+# Install once
+once init
+once domain.set localhost
+once stage next
--- once.sh.ts/src/docker/once.sh.ubuntu.22.04/entrypoint/start.sh
+++ once.sh.ts/src/docker/once.sh.ubuntu.22.04/entrypoint/start.sh
@@ -1,7 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+# The startup should not stop in case of error!
+set +e
 
 echo "Starting custom start script: $0"
+
+# Start ssh
 service ssh restart
+
+# Start ONCE server
+source ~/config/user.env
+source ~/.once
+once start
+
+# Start
 mkdir -p startmsg
 echo "Welcome to Web 4.0" >startmsg/msg.txt
 tail -f startmsg/msg.txt
