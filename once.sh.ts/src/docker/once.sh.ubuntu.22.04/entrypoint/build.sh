6a33d3263981f5dd7b64e6a819d7716cee91c0cf
Matthias Kittner <me@matthiaskittner.de>
2023-01-30 17:54:42 +0100

    Add build date; fix sshd for root login; pre run once

--- once.sh.ts/src/docker/once.sh.ubuntu.22.04/entrypoint/build.sh
+++ once.sh.ts/src/docker/once.sh.ubuntu.22.04/entrypoint/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
+mkdir -p startmsg
+NOW=`date`
+echo "This container was build: $NOW" > startmsg/build.txt
+
 echo "Starting custom build script: $PWD $0"
-export OOSH_SSH_CONFIG_HOST="dockerSSH.once2023"
+export OOSH_SSH_CONFIG_HOST="docker.once.ssh"
 
 # Temporary log settings
 #export LOG_LEVEL=3
 #export LOG_DEVICE="$HOME/build-initial-oosh.log"
-
-# Remove, when sudo in oosh is fixed
-apt install git -y
 
 # Download and install oosh
 env sh -c "$(wget -O- https://raw.githubusercontent.com/Cerulean-Circle-GmbH/once.sh/main/init/oosh)"
@@ -25,21 +26,21 @@ oo cmd net-tools
 oo cmd openssh-server
 oo cmd errno
 
-# Add test user
-#useradd -rm -d /home/ubuntu -s /bin/bash -g root -G sudo -u 1000 test 
-#echo 'test:test' | chpasswd
-#echo 'root:test' | sudo chpasswd
-
-# generate server keys
-#ssh-keygen -A
-##RUN ssh-keygen -t dsa -N "my passphrase" -C "test key" -f mykey
-##ssh-keygen -t rsa -q -f "$HOME/.ssh/id_rsa" -N ""
-
-# allow root to login
-#sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
-#sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+# Setup ssh and root login
+echo 'root:once' | chpasswd
+mkdir /var/run/sshd
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 
 # Install once
 once init
 once domain.set localhost
 once stage next
+
+# Pre start once
+once start
+once stop
+# alternatively:
+# cd <...>/EAMD.ucp/Components/tla/EAM/layer1/Thinglish/Once/latestServer
+# npm -i
