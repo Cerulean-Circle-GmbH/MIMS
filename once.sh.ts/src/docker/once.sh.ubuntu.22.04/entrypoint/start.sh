91e0f0a3b94cf1de6bd7b9dbc80bbb11491112bf
Matthias Kittner <me@matthiaskittner.de>
2023-01-30 17:55:03 +0100

    Do not start once server with container

--- once.sh.ts/src/docker/once.sh.ubuntu.22.04/entrypoint/start.sh
+++ once.sh.ts/src/docker/once.sh.ubuntu.22.04/entrypoint/start.sh
@@ -8,12 +8,7 @@ echo "Starting custom start script: $0"
 # Start ssh
 service ssh restart
 
-# Start ONCE server
-source ~/config/user.env
-source ~/.once
-once start
-
 # Start
-mkdir -p startmsg
-echo "Welcome to Web 4.0" >startmsg/msg.txt
+cat startmsg/build.txt > startmsg/msg.txt
+echo "Welcome to Web 4.0" >> startmsg/msg.txt
 tail -f startmsg/msg.txt
