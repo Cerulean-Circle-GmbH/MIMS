b930fe09c78325cd54088c6fff04efdea33d61cd
Matthias Kittner <me@matthiaskittner.de>
2023-02-15 12:16:53 +0000

    Add a image build script for all 3

---
+++ once.sh.ts/src/docker/build-jenkins-container.sh
@@ -0,0 +1,36 @@
+BRANCH=${git symbolic-ref --short -q HEAD}
+
+echo "Build all images in branch: $BRANCH"
+
+# build image once.jenkins
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
+           --entrypoint "" once.sh-builder /bin/bash -c \
+           "source ~/config/user.env &&
+           git clone 2cuGitHub:Cerulean-Circle-GmbH/Once.2023.git &&
+           cd Once.2023 &&
+           git checkout $BRANCH &&
+           cd once.sh.ts/src/docker/jenkins &&
+           ./devTool docker.build"
+
+# build image once.sh-builder
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
+           --entrypoint "" once.sh-builder /bin/bash -c \
+           "source ~/config/user.env &&
+           git clone 2cuGitHub:Cerulean-Circle-GmbH/Once.2023.git &&
+           cd Once.2023 &&
+           git checkout $BRANCH &&
+           cd once.sh.ts/src/docker/once.sh.ubuntu.22.04-builder &&
+           ./devTool docker.build"
+
+# build image once.sh-server
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
+           --entrypoint "" once.sh-builder /bin/bash -c \
+           "source ~/config/user.env &&
+           git clone 2cuGitHub:Cerulean-Circle-GmbH/Once.2023.git &&
+           cd Once.2023 &&
+           git checkout $BRANCH &&
+           cd once.sh.ts/src/docker/once.sh.ubuntu.22.04-server &&
+           ./devTool docker.build"
+
+# Example to use image interactively
+#docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock --entrypoint "" once.sh-builder /bin/bash
