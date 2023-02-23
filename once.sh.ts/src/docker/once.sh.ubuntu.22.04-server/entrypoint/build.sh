df72372e66c3662854fc62513bddda952135cfc1
Matthias Kittner <me@matthiaskittner.de>
2023-02-23 11:01:39 +0000

    Add support for local and branch installation in container

--- once.sh.ts/src/docker/once.sh.ubuntu.22.04-server/entrypoint/build.sh
+++ once.sh.ts/src/docker/once.sh.ubuntu.22.04-server/entrypoint/build.sh
@@ -8,8 +8,28 @@ echo "This container was build: $NOW" > startmsg/build.txt
 echo "Starting custom build script: $PWD $0"
 export OOSH_SSH_CONFIG_HOST="docker.once.ssh"
 
+# Import _env to see what you want to build (default: from web with branch main) (see devTool)
+if [ -f /root/entrypoint/_env ]; then
+    source /root/entrypoint/_env
+fi
+
 # Download and install oosh
-env sh -c "$(wget -O- https://raw.githubusercontent.com/Cerulean-Circle-GmbH/once.sh/main/init/oosh)"
+if [ ! -z ${OOSH_TAR} ] && [ -f ${OOSH_TAR} ]; then
+    export OOSH_INSTALL_SOURCE="/root/entrypoint/install.oosh.source"
+    echo "<build.sh> Install oosh from ${OOSH_INSTALL_SOURCE}"
+    mkdir -p ${OOSH_INSTALL_SOURCE}
+    tar xf ${OOSH_TAR} -C ${OOSH_INSTALL_SOURCE}
+    ${OOSH_INSTALL_SOURCE}/init/oosh
+else
+    if [ -z ${OOSH_BRANCH} ]; then
+        export OOSH_BRANCH="main"
+    fi
+    export OOSH_INSTALL_SOURCE="https://raw.githubusercontent.com/Cerulean-Circle-GmbH/once.sh/${OOSH_BRANCH}/init/oosh"
+    echo "<build.sh> Install oosh from ${OOSH_INSTALL_SOURCE}"
+    echo "<build.sh> Install oosh with branch ${OOSH_BRANCH}"
+    env sh -c "$(wget -O- ${OOSH_INSTALL_SOURCE})"
+fi
+
 source ~/config/user.env
 
 echo "custom build script: $PWD $0"
