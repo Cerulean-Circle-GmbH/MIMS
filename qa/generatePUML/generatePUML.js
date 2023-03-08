2be6f3a7a59f995fa7283039b2322281d5690490
Matthias Kittner <me@matthiaskittner.de>
2023-03-08 21:45:49 +0000

    Remove async stuff

--- qa/generatePUML/generatePUML.js
+++ qa/generatePUML/generatePUML.js
@@ -19,52 +19,43 @@ const directoryPath = process.argv[2];
 console.log("Path: "+directoryPath);
 
 function searchDirectoryForJsFiles(directoryPath) {
-    fs.readdir(directoryPath, (err, files) => {
-      if (err) {
-        console.log(`An error occurred while reading the directory: ${err}`);
-        return;
-      }
-  
-      // Iterate over each file or directory in the current directory
-      files.forEach(file => {
-        const filePath = path.join(directoryPath, file);
-  
-        // Check if the current file is a directory
-        if (fs.statSync(filePath).isDirectory()) {
-          // If it is a directory, recursively search for .js files inside it
-          searchDirectoryForJsFiles(filePath);
-        } else if (path.extname(filePath) === '.js') {
-          // If it is a .js file, print its name to the console
-          buffer += fs.readFileSync(filePath).toString();
-          console.log(buffer.length.toString() + ":" + filePath);
-        }
-      });
+    files = fs.readdirSync(directoryPath);
+
+    // Iterate over each file or directory in the current directory
+    files.forEach(file => {
+    const filePath = path.join(directoryPath, file);
+
+    // Check if the current file is a directory
+    if (fs.statSync(filePath).isDirectory()) {
+        // If it is a directory, recursively search for .js files inside it
+        searchDirectoryForJsFiles(filePath);
+    } else if (path.extname(filePath) === '.js') {
+        // If it is a .js file, print its name to the console
+        buffer += fs.readFileSync(filePath).toString();
+        //console.log(buffer.length.toString() + ":" + filePath);
+    }
     });
-  }
+}
 
 // Start the search in the specified directory
 searchDirectoryForJsFiles(directoryPath);
+console.log("buffer len : " + buffer.length.toString())
 
-// Nicht 5 sekunden warten, sondern auf das ergebnisse der Suche
-setTimeout(function() {
-    console.log("buffer len : " + buffer.length.toString())
-    fs.writeFile("_test.js", buffer, function(err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-}, 1000);
+fs.writeFile("_test.js", buffer, function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
 
-setTimeout(function() {
-    const jsInterpreter = new Interpreter(new Visitor())
-    const body = acorn.parse(buffer, {ecmaVersion: 2020}).body
-    //l(body)
-    fs.writeFile("_test.json", JSON.stringify(body, null, 4), function(err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-    console.log("@startuml")
-    jsInterpreter.interpret(body)
-    console.log("@enduml")
-}, 2000);
+const jsInterpreter = new Interpreter(new Visitor())
+const body = acorn.parse(buffer, {ecmaVersion: 2020}).body
+//l(body)
+fs.writeFile("_test.json", JSON.stringify(body, null, 4), function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
+
+console.log("@startuml")
+jsInterpreter.interpret(body)
+console.log("@enduml")
