fcae8ec9cbc7fb1ddd994bd1bf96287f06c706f2
Matthias Kittner <me@matthiaskittner.de>
2023-03-10 16:32:44 +0000

    Move bootstrap stuff to WODA.2023 repository

--- once.sh.ts/src/docker/once.sh.ubuntu.22.04-server/bootstrap/up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DOCKER_COMPOSE_FILE=$1
-if [[ -z $DOCKER_COMPOSE_FILE || ! -f $DOCKER_COMPOSE_FILE ]]; then
-    echo "$0 <docker-compose-file>"
-else
-    docker-compose -f $DOCKER_COMPOSE_FILE -p once up
-fi
