9b8b0c509a81d9c94cf747ea104c1d6d03bec19f
Matthias Kittner <me@matthiaskittner.de>
2023-03-31 07:58:37 +0000

    Pre Install oosh

--- once.sh.ts/src/docker/once.sh.ubuntu.22.04-server/entrypoint/start.sh
+++ once.sh.ts/src/docker/once.sh.ubuntu.22.04-server/entrypoint/start.sh
@@ -9,27 +9,18 @@ echo >> ~/startmsg/msg.txt
 echo "Timing:" >> ~/startmsg/msg.txt
 echo "start.sh:$LINENO: $(date)" >> ~/startmsg/msg.txt
 
-# https://github.com/remotemobprogramming/mob
-#curl -sL install.mob.sh | sh
-#go install github.com/remotemobprogramming/mob/v3@latest
-#echo "start.sh:$LINENO: $(date)" >> ~/startmsg/msg.txt
-
 # Start ssh
 service ssh restart
-echo "start.sh:$LINENO: $(date)" >> ~/startmsg/msg.txt
-
-# Download and install oosh
-~/entrypoint/install-oosh.sh
-echo "start.sh:$LINENO: $(date)" >> ~/startmsg/msg.txt
+echo "start.sh:$LINENO: $(date) ssh restarted" >> ~/startmsg/msg.txt
 
 # Install once (only if it is not yet initialized)
 source ~/config/user.env
 ~/entrypoint/install-once.sh
-echo "start.sh:$LINENO: $(date)" >> ~/startmsg/msg.txt
+echo "start.sh:$LINENO: $(date) ONCE installed" >> ~/startmsg/msg.txt
 
 # Setup Git configuration and SSH keys as new id in .ssh/ids
 ~/entrypoint/install-git-ssh-config.sh
-echo "start.sh:$LINENO: $(date)" >> ~/startmsg/msg.txt
+echo "start.sh:$LINENO: $(date) config integrated" >> ~/startmsg/msg.txt
 
 # Start
 echo >> ~/startmsg/msg.txt
@@ -43,4 +34,4 @@ if [ -z "$ONCE_DOCKER_HTTP_PORT" ]; then
     export ONCE_DOCKER_HTTPS_PORT=8443
 fi
 echo "and then call: http://localhost:${ONCE_DOCKER_HTTP_PORT} or https://localhost:${ONCE_DOCKER_HTTPS_PORT}" >> ~/startmsg/msg.txt
-tail -f ~/startmsg/msg.txt
+tail -n 20 -f ~/startmsg/msg.txt
