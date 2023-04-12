7683a600bab8189ba25f04209395aabf33ba4511
Matthias Kittner <me@matthiaskittner.de>
2023-04-12 09:52:09 +0200

    New version of container; Expose reverse proxy

--- once.sh.ts/src/docker/once.sh.ubuntu.22.04-server/entrypoint/install-once.sh
+++ once.sh.ts/src/docker/once.sh.ubuntu.22.04-server/entrypoint/install-once.sh
@@ -17,9 +17,20 @@ if [[ -n ${ONCE_INITIALIZED} ]]; then
     once stop
 
     # Adapt once config
+    if [ -z $ONCE_DOCKER_REVERSE_PROXY_HTTPS_PORT ]; then
+        export ONCE_DOCKER_REVERSE_PROXY_HTTPS_PORT=5005
+    fi
+    if [ -z $ONCE_DOCKER_REVERSE_PROXY_HTTP_PORT ]; then
+        export ONCE_DOCKER_REVERSE_PROXY_HTTP_PORT=5002
+    fi
     source /root/.once
     export ONCE_REVERSE_PROXY_CONFIG='[["auth","test.wo-da.de"],["snet","test.wo-da.de"],["structr","test.wo-da.de"]]'
+    export ONCE_REV_PROXY_HOST="0.0.0.0"
+    export ONCE_STRUCTR_SERVER="https://localhost:${ONCE_DOCKER_REVERSE_PROXY_HTTPS_PORT}"
+
     CF=$ONCE_DEFAULT_SCENARIO/.once
     mv $CF $CF.ORIG
-    cat $CF.ORIG | line replace "ONCE_REVERSE_PROXY_CONFIG=.*" "ONCE_REVERSE_PROXY_CONFIG='$ONCE_REVERSE_PROXY_CONFIG'" > $CF
+    cat $CF.ORIG | sed "s;ONCE_REVERSE_PROXY_CONFIG=.*;ONCE_REVERSE_PROXY_CONFIG='$ONCE_REVERSE_PROXY_CONFIG';" | \
+                   sed "s;ONCE_REV_PROXY_HOST=.*;ONCE_REV_PROXY_HOST='$ONCE_REV_PROXY_HOST';" | \
+                   sed "s;ONCE_STRUCTR_SERVER=.*;ONCE_STRUCTR_SERVER='$ONCE_STRUCTR_SERVER';" > $CF
 fi
