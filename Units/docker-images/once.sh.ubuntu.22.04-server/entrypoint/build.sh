333be62df12fd89c2a17d81244801d0e26174004
Matthias Kittner <me@matthiaskittner.de>
2024-08-10 19:50:05 +0000

    Fix version and update image

--- Units/docker-images/once.sh.ubuntu.22.04-server/entrypoint/build.sh
+++ Units/docker-images/once.sh.ubuntu.22.04-server/entrypoint/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 mkdir -p ~/startmsg
-NOW=`date`
+NOW=$(date)
 echo "This container was build: $NOW" > ~/startmsg/build.txt
 
 echo "Starting custom build script: $PWD $0"
@@ -19,7 +19,7 @@ curl -sL install.mob.sh | sh
 
 # Setup ssh and root login
 echo 'root:once' | chpasswd
-mkdir /var/run/sshd
+mkdir -p /var/run/sshd
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
