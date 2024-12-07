47d28d6124c467583c08f740edb20c0b460193fc
Chris Daßler <chris.dassler@me.com>
2024-12-07 23:58:40 +0000

    Add MinIO component

---
+++ Components/io/min/2024-11-07T00-52-20Z/scenario.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+# 'source' isn't available on all systems, so use . instead
+. .env
+. deploy-tools.sh
+
+# Set some variables
+function setEnvironment() {
+  deploy-tools.setEnvironment
+}
+
+function checkAndCreateDataVolume() {
+  banner "Check data volume"
+  deploy-tools.checkAndCreateDataVolume SCENARIO_DATA_VOLUME_1
+}
+
+function up() {
+  # Check network
+  deploy-tools.checkAndCreateNetwork $SCENARIO_SERVER_NETWORK_NAME
+
+  # Check data volume
+  checkAndCreateDataVolume
+
+  deploy-tools.up
+}
+
+function start() {
+  # Check data volume
+  checkAndCreateDataVolume
+
+  deploy-tools.start
+}
+
+function stop() {
+  # Check data volume
+  checkAndCreateDataVolume
+
+  deploy-tools.stop
+}
+
+function down() {
+  # Check data volume
+  checkAndCreateDataVolume
+
+  deploy-tools.down
+}
+
+function test() {
+  # Check data volume
+  checkAndCreateDataVolume
+
+  # Set environment
+  setEnvironment
+
+  # Print volumes, images, containers and files
+  if [ "$VERBOSITY" = "-v" ]; then
+    banner "Test"
+    log "Volumes:"
+    docker volume ls | grep ${SCENARIO_DATA_VOLUME_1_PATH}
+    log ""
+    log "Images:"
+    docker image ls | grep minio
+    log ""
+    log "Containers:"
+    docker ps -all | grep ${SCENARIO_NAME}_minio_container
+  fi
+
+  # Check MinIO status
+  banner "Check MinIO $SCENARIO_SERVER_NAME - $SCENARIO_NAME"
+  deploy-tools.checkContainer "MinIO (docker)" ${SCENARIO_NAME}_minio_container
+  return $? # Return the result of the last command
+}
+
+function logs() {
+  # Check data volume
+  checkAndCreateDataVolume
+
+  deploy-tools.logs
+}
+
+# Scenario vars
+if [ -z "$1" ]; then
+  deploy-tools.printUsage
+  exit 1
+fi
+
+STEP=$1
+shift
+
+deploy-tools.parseArguments $@
+
+if [ $STEP = "up" ]; then
+  up
+elif [ $STEP = "start" ]; then
+  start
+elif [ $STEP = "stop" ]; then
+  stop
+elif [ $STEP = "down" ]; then
+  down
+elif [ $STEP = "test" ]; then
+  test
+elif [ $STEP = "logs" ]; then
+  logs
+else
+  deploy-tools.printUsage
+  exit 1
+fi
+
+exit $?
